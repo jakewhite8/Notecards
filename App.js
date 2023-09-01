64efb26b9cdc977e8d6e4b174d21217c43cb9336
Jake White <jwhite52@gmail.com>
2023-08-31 21:48:08 -0400

    Add React Native Elements UI toolkit

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Button, createTheme, ThemeProvider } from '@rneui/themed';
+
+const theme = createTheme({
+  lightColors: {
+    primary: '#e7e7e8',
+  },
+  darkColors: {
+    primary: '#000',
+  },
+  mode: 'dark',
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Notecard App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <View style={styles.container}>
+          <Text>Notecard 3</Text>
+          <Button title="Button 3" />
+          <StatusBar style="auto" />
+        </View>
+      </ThemeProvider >
+    </SafeAreaProvider>
   );
 }
 
