0025fbbaed93efc67564a270f0b927d6b3ecfd6b
Jake White <jwhite52@gmail.com>
2023-09-28 22:57:37 -0400

    Add list of notecards to Home page

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,6 +1,11 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button } from '@rneui/themed';
+import { 
+  Button,
+  ListItem,
+  ListItemProps,
+} from '@rneui/themed';
+import { LinearGradient } from 'expo-linear-gradient'
 import { useContext } from 'react';
 import { UsernameReducerContext } from '../helpers/UsernameReducer';
 
@@ -19,6 +24,30 @@ const styles = StyleSheet.create({
   },
 });
 
+type NotecardData = {
+  name: string;
+  linearGradientColors: string[];
+};
+
+const notecards: Partial<NotecardData>[] = [
+  {
+    name: 'Computer Science',
+    linearGradientColors: ['#FF9800', '#F44336'],
+  },
+  {
+    name: 'Carpentry',
+    linearGradientColors: ['#3F51B5', '#2196F3'],
+  },
+  {
+    name: 'French',
+    linearGradientColors: ['#FFD600', '#FF9800'],
+  },
+  {
+    name: 'CSL',
+    linearGradientColors: ['#4CAF50', '#8BC34A'],
+  },
+];
+
 // Used to type check the Screen components.
 // This allows us to type check route names and params used by
 // the navigate and push functions, etc.
@@ -28,18 +57,47 @@ type HomeProps = NativeStackScreenProps<StackParamList, 'Home'>;
 function Home( { navigation }: HomeProps) {
 
   const { UsernameState } = useContext(UsernameReducerContext);
+  const listItemProps = {};
 
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Text>Username: {UsernameState.username}</Text>
-      <Button
-        title="Details Page"
-        onPress={() =>
-          navigation.navigate('Details', {name: 'Custom Details header'})
-        } />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Hello {UsernameState.username}</Text>
+        <Button
+          title="Details Page"
+          onPress={() =>
+            navigation.navigate('Details', {name: 'Custom Details header'})
+          } />
+      </View>
+      <View style={{ paddingVertical: 8 }}>
+        {notecards.map((notecard, i) => (
+          <ListItem
+            {...(listItemProps as ListItemProps)}
+            key={i}
+            linearGradientProps={{
+              colors: notecard.linearGradientColors,
+              start: [1, 0],
+              end: [0.2, 0],
+            }}
+            ViewComponent={LinearGradient}
+            containerStyle={{
+              marginHorizontal: 16,
+              marginVertical: 8,
+              borderRadius: 8,
+            }}
+          >
+            <ListItem.Content>
+              <ListItem.Title
+                style={{ color: 'white', fontWeight: 'bold' }}
+              >
+                {notecard.name}
+              </ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Chevron color="white" />
+          </ListItem>
+        ))}
+      </View>
+    </>
   )
 }
 
