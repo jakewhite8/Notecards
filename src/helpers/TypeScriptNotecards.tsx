b2faec17c380519ef2cc916e9f0df23bbfbca336
Jake White <jwhite52@gmail.com>
2023-10-04 20:34:16 -0400

    Create sample notecard

---
+++ src/helpers/TypeScriptNotecards.tsx
@@ -0,0 +1,21 @@
+const TypeScriptNotecards = {
+  title: 'TypeScript',
+  notecards: [{
+    front: 'What is TypeScript?',
+    back: 'Definition: TypeScript is a statically typed programming language developed by Microsoft. It builds upon JavaScript by adding static type checking, interfaces, and other features to help developers write more robust and maintainable code. Key Feature: Static typing helps catch errors at compile-time and improves code quality.'
+  }, {
+    front: 'TypeScript and JavaScript Compatibility',
+    back: 'Feature: TypeScript is compatible with JavaScript. You can gradually adopt TypeScript by renaming .js files to .ts and adding type annotations as needed. Example: JavaScript code can be used within TypeScript files, and TypeScript code can be transpiled to JavaScript.'
+  }, {
+    front: 'Type Annotations and Declarations',
+    back: "Feature: TypeScript uses type annotations and declarations to specify the data types of variables, function parameters, and return values. Example: let age: number = 25; declares a variable age as a number type."
+  }, {
+    front: 'Interfaces and Type Definitions',
+    back: 'TypeScript introduces interfaces and type definitions to define the shape of objects or custom types.'
+  }, {
+    front: 'TypeScript Compiler',
+    back: 'Feature: TypeScript code is transpiled into JavaScript using the TypeScript compiler (tsc). It checks for type errors and generates JavaScript files that can run in browsers or Node.js. Usage: Run tsc followed by the filename to compile TypeScript files into JavaScript.'
+  }]
+}
+
+export default TypeScriptNotecards
