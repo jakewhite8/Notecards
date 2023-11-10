c7efa4f25a879ae9b89566310886a97afaef3e75
Jake White <jwhite52@gmail.com>
2023-11-09 19:32:32 -0500

    Add styles to createTitle page

--- src/views/createTitle.tsx
+++ src/views/createTitle.tsx
@@ -10,6 +10,7 @@ import {
   Icon,
 } from '@rneui/themed';
 import { useAppState } from '../context/GlobalState';
+import { useTheme } from '@rneui/themed';
 
 type CreateProps = NativeStackScreenProps<StackParamList, 'CreateTitle'>
 
@@ -18,6 +19,7 @@ const styles = GlobalStyles;
 function CreateTitle ({ navigation }: CreateProps) {
   const [newTitle, setTitle] = useState('');
   const { state, dispatch } = useAppState();
+  const { theme } = useTheme();
 
   interface WrappedInputProps extends InputProps {
     ref?: React.RefObject<TextInput & BaseInput>;
@@ -36,23 +38,24 @@ function CreateTitle ({ navigation }: CreateProps) {
   }
 
   return (
-    <View style={styles.container}>
-      <Text>{newTitle}</Text>
+    <View style={[styles.container, {backgroundColor: theme.colors.secondaryBackground}]}>
+      <Text style={{color: theme.colors.primaryText}}>{newTitle}</Text>
       <Input
         {...(inputProps as WrappedInputProps)}
         rightIcon={
           <Icon
             name="chevron-right"
             type="entypo"
-            color="#86939e"
+            color={theme.colors.primaryText}
             size={25}
             onPress={submitNotecardSetTitle}
           />
         }
-        label="New Notecard Set"
+        label="New Notecard Set Title"
+        labelStyle={{color: theme.colors.primaryText}}
         containerStyle={{ width: '90%' }}
         placeholder="Name"
-        style={styles.inputFieldsStyle}
+        style={[styles.inputFieldsStyle, {color: theme.colors.primaryText}]}
         onChangeText={(title) => setTitle(title)}
       />
     </View>
