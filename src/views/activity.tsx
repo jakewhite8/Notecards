3ca249387561d3a6be3e84c412508653eef59b90
Jake White <jwhite52@gmail.com>
2024-01-31 20:05:30 -0500

    Query Notecard data from InfluxDB

--- src/views/activity.tsx
+++ src/views/activity.tsx
@@ -13,14 +13,15 @@ import SampleActivityData from '../helpers/SampleActivityData';
 const styles = GlobalStyles;
 const sampleData = SampleActivityData;
 
+const token = '<influxdb_token>';
 const org = "Notecards";
-const bucket = "get-started";
+const bucket = "activity";
 const url = "https://us-east-1-1.aws.cloud2.influxdata.com/";
 
-let query = `from(bucket: "get-started")
+let query = `from(bucket: "activity")
   |> range(start: -720h)
-  |> filter(fn: (r) => r["_measurement"] == "home")
-  |> filter(fn: (r) => r["_field"] == "co")`;
+  |> filter(fn: (r) => r["_measurement"] == "notecard")
+  |> filter(fn: (r) => r["_field"] == "userId")`;
 
 type ActivityProps = NativeStackScreenProps<StackParamList, 'Activity'>;
 
@@ -28,6 +29,7 @@ function Activity( { navigation, route }: ActivityProps) {
   const { theme } = useTheme();
   const { state, dispatch } = useAppState();
   const {t, i18n} = useTranslation();
+  const [influxData, setInfluxData] = useState([]);
 
   useEffect(() => {
     let res = [];
@@ -41,15 +43,13 @@ function Activity( { navigation, route }: ActivityProps) {
           const o = tableMeta.toObject(row);
          //push rows from query into an array object
           res.push(o);
-          // console.log(res)
         },
         complete() {
-          
           console.log('res');
           console.log(res);
           console.log('res.length');
           console.log(res.length);
-
+          setInfluxData(res)
         },
         error(error) {
           console.log("query failed- ", error);
@@ -64,7 +64,7 @@ function Activity( { navigation, route }: ActivityProps) {
     <View style={{backgroundColor: theme.colors.secondaryBackground, flex: 1}}>
         <View style={{ flexDirection:'row'} }>
           <View style={{flex: 0.33}}>
-            <AnalyticTextField title="Total Cards Viewed" value={sampleData.cardsViewed}/>
+            <AnalyticTextField title="Total Cards Viewed" value={influxData.length}/>
           </View>
           <View style={{flex: 0.33}}>
             <AnalyticTextField title="Current View Streak" value={sampleData.currentViewStreak}/>
